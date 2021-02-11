524d810410d4ae8fb64e86d8726dfa6b62234c85
patricksoftwaredev <PatrykBarth@mail.com>
2021-02-11 13:45:33 +0000

    added a Materialize Registration Form

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, flash, render_template, redirect, request, session, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -14,11 +15,17 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+
 @app.route("/")
 @app.route("/tasks")
 def tasks():
     tasks = mongo.db.tasks.find()
     return render_template("tasks.html", tasks=tasks)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
