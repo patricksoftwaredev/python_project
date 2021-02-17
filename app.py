714166f4750e78b1016f34b8db89a2dce6868246
patricksoftwaredev <PatrykBarth@mail.com>
2021-02-17 11:03:42 +0000

    update

--- app.py
+++ app.py
@@ -53,12 +53,12 @@ def login():
         existing_user = mongo.db.users.find_one(
             {"email": request.form.get("email").lower()})
 
-          if existing_user:
+        if existing_user:
                if check_password_hash(
                         existing_user["password"], request.form.get("pasword")):
                     session["user"] = request.form.get("email").lower()
                     print("Welcome,{}".format(request.form.get("email")))
-                else:
+        else:
                     print("incorrect Name or Password")
                     return redirect(url_for("login"))
 
